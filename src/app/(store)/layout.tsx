cf60692c10f9a5d64490729b2b2f9679cfa8d3a3
viitones <vitonesmartins@gmail.com>
2024-10-10 23:53:05 -0300

    contexto do carrinho

--- src/app/(store)/layout.tsx
+++ src/app/(store)/layout.tsx
@@ -1,11 +1,14 @@
+import { CartProvider } from '@/context/cart-context'
 import { ReactNode } from 'react'
 import { Header } from '../components/header'
 
 export default function StoreLayout({ children }: { children: ReactNode }) {
   return (
-    <div className="mx-auto grid grid-rows-[min-content_max-content] min-h-screen w-full max-w-[1600px] gap-5 p-8">
-      <Header />
-      {children}
-    </div>
+    <CartProvider>
+      <div className="mx-auto grid grid-rows-[min-content_max-content] min-h-screen w-full max-w-[1600px] gap-5 p-8">
+        <Header />
+        {children}
+      </div>
+    </CartProvider>
   )
 }
