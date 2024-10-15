469191dec68ccb6cb1d492ebc1757d9591429b2a
viitones <vitonesmartins@gmail.com>
2024-10-14 21:33:43 -0300

    teste customizado

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -9,29 +9,15 @@
 // https://on.cypress.io/custom-commands
 // ***********************************************
 //
+declare namespace Cypress {
+  interface Chainable {
+    searchByQuery(query: string): Chainable<void>
+  }
+}
 //
 // -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
-//
-// declare global {
-//   namespace Cypress {
-//     interface Chainable {
-//       login(email: string, password: string): Chainable<void>
-//       drag(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
-//       dismiss(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
-//       visit(originalFn: CommandOriginalFn, url: string, options: Partial<VisitOptions>): Chainable<Element>
-//     }
-//   }
-// }
+Cypress.Commands.add('searchByQuery', (query: string) => {
+  cy.visit('/')
+
+  cy.get('input[name=q]').type(query).parent('form').submit()
+})
